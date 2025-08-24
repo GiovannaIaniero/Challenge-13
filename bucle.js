e031754ffbeeef91417b16fd0710aaf46a2ecfa9
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2025-08-24 20:19:10 -0300

    terminé ejercicio

--- bucle.js
+++ bucle.js
@@ -1,3 +1,8 @@
 /*Bucle del 1 al 20
 Crea un bucle que imprima los números del 1 al 20*/
 
+let num = 1;
+
+for (let num = 1; num <= 20; num++) {
+    document.writeln("El valor del número es: " + num + "<br>");
+}
