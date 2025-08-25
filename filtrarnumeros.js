0041179b4e90d594437caf23e4bc9e2cb5c98f78
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2025-08-24 21:12:30 -0300

    vinculé link js en html

---
+++ filtrarnumeros.js
@@ -0,0 +1,14 @@
+/*Dado el array:
+ let numerosMayoresA10 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]; 
+ Usar un bucle para crear un nuevo array con solo los números mayores a 10.*/
+let numerosMayoresA10 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]; 
+let array = [9];
+
+
+for (let i = 0; array < numerosMayoresA10.length; i++) {
+    if (numerosMayoresA10[i] > [9]) {
+        mayores.push(numerosMayoresA10[i]); 
+}
+
+
+document.wrieln("Los números mayores a 10 son: " + mayores);
