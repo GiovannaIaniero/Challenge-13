cea387a23fc559901b3b2eb4565ec1bfee3b84e2
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2025-08-24 22:03:04 -0300

    inicio ejercicio

---
+++ imc.js
@@ -0,0 +1,11 @@
+/*Cálculo de IMC
+Diseñar un algoritmo que calcule el IMC con peso (kg) y altura (m).
+Mostrar el resultado con su categoría:
+Menos de 18.5 → Peso bajo
+Entre 18.5 y 24.99 → Peso normal
+25 o más → Sobrepeso
+Fórmula:
+imc = peso / (altura ** 2);*/
+
+let peso = parseFloat(prompt("Ingrese su peso:"));
+let altura = parseFloat(prompt("Ingrese su altura"));
